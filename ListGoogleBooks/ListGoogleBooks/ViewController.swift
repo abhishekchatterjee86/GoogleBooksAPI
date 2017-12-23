c79634b549cf6a0291341f31f1bb3e1879775001
Abhishek Chatterjee <abhishek.chatterjee@philips.com>
2017-12-23 15:41:58 +0530

    Added code to display books. Configured table cell

--- ListGoogleBooks/ListGoogleBooks/ViewController.swift
+++ ListGoogleBooks/ListGoogleBooks/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allBooks: [Book] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.estimatedRowHeight = 68.0
         self.tableView.rowHeight = UITableViewAutomaticDimension
+
+        self.fetchList()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,17 +26,42 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Fetch Books
+    func fetchList() {
+        let session = URLSession.shared
+        let client = GoogleBooksApiClient(session: session)
+
+        let req = GoogleBooksApi.VolumeRequest.List(query: "Cormen")
+        let task: URLSessionDataTask = client.invoke(
+            req,
+            onSuccess: { volumes in 
+                self.allBooks = volumes.items
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+        },
+            onError: { error in NSLog("\(error)") }
+        )
+        task.resume()
+    }
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.allBooks.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BooksTableViewCell
+
+        let volume = self.allBooks[indexPath.row]
+
+        cell.bookTitle.text = volume.volumeInfo.title
+
+        cell.bookAuthor.text = volume.volumeInfo.authors.joined(separator: ",")
 
         return cell
     }
